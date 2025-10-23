b55929ac702946303c1190eb5dc15fe4ff97ee18
rshyam02 <114812555+rshyam02@users.noreply.github.com>
2025-10-23 19:44:33 +0530

    Update App.js

--- my-project/client/frontend/src/App.js
+++ my-project/client/frontend/src/App.js
@@ -1,31 +1,26 @@
-import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
-import Login from './Login.js'
-import Home from './Home.js'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './Login.js';
+import Home from './Home.js';
 import Task_List from './Task_List.js';
 import Add_Task from './Add_Task.js';
 import Delete_Task from './Delete_Task.js';
 import Completed_Task from './Completed_Task.js';
 import Edit_Task from './Edit_Task.js';
 
-
 import './App.css';
 
 function App() {
   return (
-    // <Login></Login>
     <Router>
       <Routes>
-        <Route path="/" Component={Home}></Route>
-        
-        <Route path="/Task_List" Component={Task_List}></Route>
-        <Route path="/Add_Task" Component={Add_Task}></Route>
-        <Route path="/Delete_Task" Component={Delete_Task}></Route>
-        <Route path="/Completed_Task" Component={Completed_Task}></Route>
-        <Route path="/EditTasks" Component={Edit_Task}></Route>
-        <Route path="./EditTasks/:taskId"Component={Edit_Task}></Route>
-
-
-        
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/task_list" element={<Task_List />} />
+        <Route path="/add_task" element={<Add_Task />} />
+        <Route path="/delete_task" element={<Delete_Task />} />
+        <Route path="/completed_task" element={<Completed_Task />} />
+        <Route path="/edit_task" element={<Edit_Task />} />
+        <Route path="/edit_task/:taskId" element={<Edit_Task />} />
       </Routes>
     </Router>
   );
