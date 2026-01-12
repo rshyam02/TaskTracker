c8c2d96f0550b33eff0f54728fdbc56c5a88a098
rshyam02 <114812555+rshyam02@users.noreply.github.com>
2026-01-12 19:43:44 +0530

    Update login_authentication.js

--- my-project/server/backend/login_authentication.js
+++ my-project/server/backend/login_authentication.js
@@ -1,18 +1,35 @@
+const { response } = require("express");
+const mongoose=require("mongoose");
+const bcrypt = require('bcryptjs');
+const jwt=require("jsonwebtoken");
 
-const userdetails=[
-    {username:'user1', password:'pass1'},
-    {username:'user2', password:'pass2'}
-];
+const mongouri = "mongodb+srv://shyam:shyam02@cluster0.vojjf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(mongouri,{useNewUrlParser:true,useUnifiedTopology:true,serverSelectionTimeoutMS:30000,socketTimeoutMS:60000})
+    .then(()=>console.log("mongodb connected successfully"))
+    .catch((err)=>console.log("mongodb connection error "+err));
 
-const authentication=(username,password)=>{
-    const user=userdetails.find(i=>i.username===username && i.password===password);
-    console.log(user);
-    if(user){
-        return "Login Successful";        
+const loginSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  email: String
+});
+
+const Login = mongoose.models.Login || mongoose.model("login_details", loginSchema);
+
+
+const authentication=async(username,password)=>{
+    const user=await Login.findOne({username});
+    if(!user){
+        return false;
     }
-    else{
-        return "Invalid Details";
+    const password_match=await bcrypt.compare(password,user.password);
+    if(!password_match){
+        return false;
     }
-};
+    const token=jwt.sign({id:user._id},"secret123",{expiresIn:"1h"});
+    return token;
+    
+ }
+
+
 module.exports={authentication};
-
